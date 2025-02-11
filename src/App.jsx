8c04ad6059e4770da4e794115f7e9ded5cc6840c
Misha Androshchuk <androsukmisa3@gmail.com>
2025-02-11 22:21:19 +0200

    add navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import Login from './pages/authPage/login/Login.jsx';
 import Register from './pages/authPage/register/Register.jsx';
 import MainPage from './pages/mainPage/mainPage.jsx';
+import NotFound from './pages/notFound/NotFound';
+import DefaulLayout from './components/layout/DefaultLayout';
 
 function App() {
   return (
     <GoogleOAuthProvider clientId="47235399203-5dbvs4krmn7oao0p2fk1102dpam9vgsb.apps.googleusercontent.com">
       <Routes>
-        <Route path="/log" element={<Login />} />
-        <Route path="/reg" element={<Register />} />
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<DefaulLayout />} >
+          <Route index element={<MainPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </GoogleOAuthProvider>
   );
